c575d9706096deda232abf28e019ac28d317a4a8
Viccho2828 <kikekollizion@gmail.com>
2024-11-28 21:07:50 -0500

    splash activity finalizado

--- app/src/main/java/pe/edu/idat/apppatitasidat/view/activities/SplashActivity.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidat/view/activities/SplashActivity.kt
@@ -1,6 +1,9 @@
 package pe.edu.idat.apppatitasidat.view.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,13 +12,15 @@ import pe.edu.idat.apppatitasidat.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,
+                LoginActivity::class.java))
+            finish()
+        }, 6000)
+
     }
 }
