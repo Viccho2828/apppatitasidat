2061ff2c7b7f15421b0655a30e0e9df76bd451c5
Viccho2828 <kikekollizion@gmail.com>
2024-11-28 21:01:34 -0500

    diseño de login y registro

--- app/src/main/java/pe/edu/idat/apppatitasidat/view/activities/RegistroActivity.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidat/view/activities/RegistroActivity.kt
@@ -10,12 +10,7 @@ import pe.edu.idat.apppatitasidat.R
 class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_registro)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
