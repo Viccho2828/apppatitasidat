ca1c10f65c72ab5c99740a78dc1f8207e1562532
Viccho2828 <kikekollizion@gmail.com>
2024-11-28 22:29:00 -0500

    viewmodel terminado

--- app/src/main/java/pe/edu/idat/apppatitasidat/viewmodel/AuthViewModel.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidat/viewmodel/AuthViewModel.kt
@@ -1,4 +1,41 @@
 package pe.edu.idat.apppatitasidat.viewmodel
 
-class AuthViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import pe.edu.idat.apppatitasidat.repository.AuthRepository
+import pe.edu.idat.apppatitasidat.retrofit.request.RequestLogin
+import pe.edu.idat.apppatitasidat.retrofit.request.RequestRegistro
+import pe.edu.idat.apppatitasidat.retrofit.response.ResponseLogin
+import pe.edu.idat.apppatitasidat.retrofit.response.ResponseRegistro
+
+class AuthViewModel : ViewModel() {
+
+    var responseLogin: LiveData<ResponseLogin>
+    var responseRegistro: LiveData<ResponseRegistro>
+    private var repository = AuthRepository()
+
+    init {
+        responseLogin = repository.loginResponse
+        responseRegistro = repository.registroResponse
+    }
+
+    fun autenticarUsuario(usuario: String,
+                          password: String){
+        responseLogin = repository.autenticarUsuario(
+            RequestLogin(usuario, password))
+    }
+
+    fun registroUsuario(nombres: String,
+                        apellidos: String,
+                        email: String,
+                        celular: String,
+                        usuario: String,
+                        password: String){
+        responseRegistro = repository.registrarUsuario(
+            RequestRegistro(nombres,apellidos, email,
+                celular, usuario, password)
+        )
+    }
+
+
 }
