9a12528d61cf6a223b9280125bb48da0bd7bfdc6
Viccho2828 <kikekollizion@gmail.com>
2024-12-05 21:58:48 -0500

    viewmodel terminado

--- app/src/main/java/pe/edu/idat/apppatitasidat/viewmodel/MascotaViewModel.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidat/viewmodel/MascotaViewModel.kt
@@ -1,4 +1,20 @@
 package pe.edu.idat.apppatitasidat.viewmodel
 
-class MascotaViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import pe.edu.idat.apppatitasidat.repository.MascotaRepository
+import pe.edu.idat.apppatitasidat.retrofit.response.ResponseMascota
+import pe.edu.idat.apppatitasidat.retrofit.response.ResponseRegistro
+
+class MascotaViewModel : ViewModel() {
+    private var repository = MascotaRepository()
+    var responseRegistro: LiveData<ResponseRegistro>
+    = repository.responseRegistro
+    fun listarMascotas(): LiveData<List<ResponseMascota>>{
+        return repository.listarMascota()
+    }
+    fun registraVoluntario(idpersona: Int){
+        responseRegistro = repository
+            .registrarVoluntario(idpersona)
+    }
 }
