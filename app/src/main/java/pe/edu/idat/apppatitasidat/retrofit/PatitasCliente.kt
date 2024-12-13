9de87f7444016796414e80497a67c6640e9672fb
Viccho2828 <kikekollizion@gmail.com>
2024-12-12 21:23:12 -0500

    Interceptor Token

--- app/src/main/java/pe/edu/idat/apppatitasidat/retrofit/PatitasCliente.kt
+++ app/src/main/java/pe/edu/idat/apppatitasidat/retrofit/PatitasCliente.kt
@@ -1,12 +1,22 @@
 package pe.edu.idat.apppatitasidat.retrofit
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object PatitasCliente {
 
+    private var okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(RetrofitInterceptor())
+        .connectTimeout(1, TimeUnit.MINUTES)
+        .readTimeout(1, TimeUnit.MINUTES)
+        .writeTimeout(1, TimeUnit.MINUTES)
+        .build()
+
     private fun buildRetrofit() = Retrofit.Builder()
         .baseUrl("https://app.juankuga.com/wspatitas/")
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
---
+++ app/src/main/java/pe/edu/idat/apppatitasidat/retrofit/RetrofitInterceptor.kt
@@ -0,0 +1,17 @@
+package pe.edu.idat.apppatitasidat.retrofit
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class RetrofitInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain):
+            Response {
+        val token = ""
+        val request = chain.request().newBuilder()
+            .addHeader(
+                "Authorization",
+                "Bearer "+token).build()
+        return chain.proceed(request)
+    }
+}
